2c6ae49e41f48b3c00223c41024a07cb088497af
aysenurbakirciobilet <aysenur.bakirci@obilet.com>
2024-11-22 18:33:32 +0300

    Add package

---
+++ Package.swift
@@ -0,0 +1,22 @@
+import PackageDescription
+
+ let package = Package(
+     name: "DangerExample-PRLinter",
+     defaultLocalization: "en",
+     products: [
+       .library(name: "DangerDeps", type: .dynamic, targets: ["DangerExample-PRLinter"])
+     ],
+     dependencies: [
+         .package(url: "https://github.com/danger/swift.git", from: "3.12.3")
+     ],
+     targets: [
+         .target(
+             name: "DangerExample-PRLinter",
+             dependencies: [
+                 .product(name: "Danger", package: "swift")
+             ],
+             path: "DangerExample",
+             sources: ["Stringfy.swift"]
+         )
+     ]
+ )
