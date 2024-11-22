2898cdfac6c3a885374cdefa4312ea4e42d5a507
aysenurbakirciobilet <aysenur.bakirci@obilet.com>
2024-11-22 19:39:13 +0300

    Add package

--- Package.swift
+++ Package.swift
@@ -1,23 +1,25 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
- let package = Package(
-     name: "DangerExample-PRLinter",
-     defaultLocalization: "en",
+let package = Package(
+    name: "DangerExample",
+    defaultLocalization: "en",
      products: [
-       .library(name: "DangerDeps", type: .dynamic, targets: ["DangerExample-PRLinter"])
+       .library(name: "DangerDeps", type: .dynamic, targets: ["DangerExample"])
      ],
-     dependencies: [
-         .package(url: "https://github.com/danger/swift.git", from: "3.12.3")
+    dependencies: [
+         .package(url: "https://github.com/danger/swift.git")
      ],
-     targets: [
-         .target(
-             name: "DangerExample-PRLinter",
+    targets: [
+        .target(
+             name: "DangerExample",
              dependencies: [
                  .product(name: "Danger", package: "swift")
              ],
              path: "DangerExample",
              sources: ["Stringfy.swift"]
          )
-     ]
- )
+    ]
+)
