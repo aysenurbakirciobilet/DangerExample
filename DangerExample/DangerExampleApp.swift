10a73e85193aa9380e72338a5b79a16896689ef5
aysenurbakirciobilet <aysenur.bakirci@obilet.com>
2024-11-22 17:22:46 +0300

    İnitial commit

---
+++ DangerExample/DangerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DangerExampleApp.swift
+//  DangerExample
+//
+//  Created by Ayşe Nur Bakıcı on 22.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct DangerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DangerExampleTests/DangerExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  DangerExampleTests.swift
+//  DangerExampleTests
+//
+//  Created by Ayşe Nur Bakıcı on 22.11.2024.
+//
+
+import Testing
+@testable import DangerExample
+
+struct DangerExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
